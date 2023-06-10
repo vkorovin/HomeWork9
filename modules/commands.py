25c4d8cc0a358784b9bab67bd2872d63b4392015
Vasily Korovin <vskorovin@gmail.com>
2023-06-10 09:07:57 +0300

    New data modules

--- modules/commands.py
+++ modules/commands.py
@@ -7,23 +7,23 @@ class Command():
         self.help = help
 
 
-    def action(self,**params):
-        pass
+    def action(self,*param):
+        print(param)
 
-    def  getdescr(self):
+    def  get_descr(self):
         return self.command, self.help
 
 
 class CommandAdd(Command):
     def action(self, *param):
         self.data_source.add(param[0],param[1])
-        print("")
+        self.data_source.save()
 
 
 class CommandDone(Command):
     def action(self, *param):
         self.data_source.delete(param[0])
-
+        self.data_source.save()
 
 class CommandShow(Command):
     def action(self, *param):
@@ -35,7 +35,30 @@ class CommandShow(Command):
         for item in dataset:
             print("{0} \t\t {1}".format(item['title'],item['description']))
 
+
+class CommandSearch(Command):
+    def action(self,*param):
+        index_list1 = self.data_source.search('title',param[0])
+        index_list2 = self.data_source.search('description', param[0])
+
+        for item in index_list2:
+            try:
+                index_list1.index(item)
+            except:
+                index_list1 += (item,)
+        print('Found {0} tasks:'.format(len(index_list1)))
+        print('Title \t\t\t Description')
+        print('==============================')
+
+        for item in index_list1:
+            record = self.data_source.get(item)
+            print("{0} \t\t {1}".format(record[0]['title'], record[0]['description']))
+
+
+
+
 if __name__ == '__main__':
     ds = datasource.DataCollection()
-    cmd = CommandShow(ds,'show','Show N fresh tasks')
+    cmd = CommandSearch(ds,'show','Show N fresh tasks')
+    cmd.action('Title')
 
