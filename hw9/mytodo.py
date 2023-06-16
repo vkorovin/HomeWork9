8b0ea9f4745a56f298f73fc3f42c25e1d230e1df
Vasily Korovin <vskorovin@gmail.com>
2023-06-16 12:16:58 +0300

    Some types annotations

--- hw9/mytodo.py
+++ hw9/mytodo.py
@@ -2,6 +2,22 @@ from hw9 import  commands as cmd
 import argparse
 
 
+def convert_args(fake_kwargs) -> list:
+
+    myargs = list()
+
+    for item in fake_kwargs:
+        if item[0] == 'command':
+            continue
+        if type(item[1]) == list and len(item[1]) == 1:
+            value, =item[1]
+        else:
+            value = item[1]
+        myargs.append(value)
+
+    return myargs
+
+
 def run() -> None:
     cmd_dict = cmd.CommanndsDict()
 
@@ -9,12 +25,14 @@ def run() -> None:
     subcmd = parser.add_subparsers(dest='command', required=True, metavar='command')
 
     for cmd_name, cmd_descr, cmd_atype  in cmd_dict.description():
-        subp = subcmd.add_parser(cmd_name,help = cmd_descr)
+        subp = subcmd.add_parser(cmd_name, help = cmd_descr)
         for cmd_arg in cmd_atype:
-            subp.add_argument(cmd_arg[0],nargs=1,type=cmd_arg[1])
+            subp.add_argument(cmd_arg[0],nargs=1, type=cmd_arg[1])
 
     args = parser.parse_args()
-    cmd_dict.action(args.command)(*args.args)
+    fake_kwargs = args._get_kwargs()
+
+    cmd_dict.action(args.command)(*convert_args(fake_kwargs))
 
 if __name__ == '__main__':
     run()
